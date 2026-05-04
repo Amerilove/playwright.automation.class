ad76138d8f3e772e42b91e581d2f65a2faf0f663
Sofia <touhami-sofia@hotmail.fr>
2026-05-04 18:22:41 +0100

    Enhance getToken function documentation
    Refactor getToken function with comments for clarity.

--- test/api/token-client.ts
+++ test/api/token-client.ts
@@ -1,16 +1,17 @@
 //→ gets token
 import { APIRequestContext } from '@playwright/test';
-
+//Importing the APIRequestContext type to ensure my authentication function follows the strict structure required by the Playwright library for making API calls.
 export async function getToken(request: APIRequestContext) {
+    //creating a reusable, public tool that will go out to the internet, talk to a server, and bring back a token, and I'm going to use the official Playwright rules to do it
     const response = await request.post('/api/auth/token', {
         data: {
             username: 'sofiat',
             password: 'N7p#L4x!C9r@W2mK',
       },
     });
-
+//Verification: The server checks if 'sofiat' is a valid user.
     const responseToken = await response.json();
-
+//Response: The server sends back a JSON package containing your Token.
     return responseToken.token;
-    
+    //Delivery: Your function extracts that specific token and hands it to your tests.
 }
